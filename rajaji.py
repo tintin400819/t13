41ca20785f591810abcc48d24b76e3e954382b54
tintin400819 <tintin.400819@gmail.com>
2025-02-06 21:17:12 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -1,14 +1,15 @@
-# Don't use quotes( " and ' )
-#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
-  
-#Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7102257066:AAHJ7ssWRJtlbb6oKhSf61iHsZ9Gh6Gn-vM")
+# SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
 
-  #Enter Your telegram username here without @
-OWNER_USERNAME=("@Ayush143")
+# Enter Your Bot Token here (get it from @BotFather)
+BOT_TOKEN = "7102257066:AAHJ7ssWRJtlbb6oKhSf61iHsZ9Gh6Gn-vM"
 
-  #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("859818729")
+# Enter Your Telegram Usernames (without @)
+OWNER_USERNAME_1 = "Ayush143"
+OWNER_USERNAME_2 = "ClutchGodOp"
+
+# Enter Your Admin ID(s) (Get it from @missRose_bot using /info)
+ADMIN_IDS = [859818729]  # List of admin IDs (integers)
+
 
 
 
