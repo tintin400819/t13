9ed86748baba7b4e78c4457e4785e87ce7dd5cde
tintin400819 <tintin.400819@gmail.com>
2025-02-06 21:17:22 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -4,8 +4,8 @@
 BOT_TOKEN = "7102257066:AAHJ7ssWRJtlbb6oKhSf61iHsZ9Gh6Gn-vM"
 
 # Enter Your Telegram Usernames (without @)
-OWNER_USERNAME_1 = "Ayush143"
-OWNER_USERNAME_2 = "ClutchGodOp"
+OWNER_USERNAME_1 = "@Ayush143"
+OWNER_USERNAME_2 = "@ClutchGodOp"
 
 # Enter Your Admin ID(s) (Get it from @missRose_bot using /info)
 ADMIN_IDS = [859818729]  # List of admin IDs (integers)
