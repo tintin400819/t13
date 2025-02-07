1e4111820e5542efdc35505000662948221a72c3
tintin400819 <tintin.400819@gmail.com>
2025-02-07 15:18:40 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -1,17 +1,11 @@
-# Don't use quotes( " and ' )
-#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
-  
-#Enter Your Bot Token here get it from @botfarher
+# Don't use quotes(  and  )
+# SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+
+# Enter Your Bot Token here get it from @botfarher
 BOT_TOKEN=("7102257066:AAHJ7ssWRJtlbb6oKhSf61iHsZ9Gh6Gn-vM")
 
-  #Enter Your telegram username here without @
-OWNER_USERNAME=("@Ayush143")
+# Enter Your telegram usernames here without @
+OWNER_USERNAME=("@Ayush143", "@ClutchGodOp")
 
-  #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("859818729")  
-
-
-
-
-
-  
+# Enter your admin IDs here. Get it from @missRose_bot by typing /info
+ADMIN_IDS=("859818729", "787517301")
